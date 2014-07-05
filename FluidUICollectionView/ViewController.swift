a8a160aaad5aec40c2aeff6ec10913caa51d3323
Michał Smulski <smulski.michal@gmail.com>
2014-07-05 15:12:19 +0200

    Initial Commit

---
+++ FluidUICollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FluidUICollectionView
+//
+//  Created by Michał Smulski on 05.07.2014.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
